7c6ece8f2bb717fc5251cd983e871c11fe79908e
piosterk <147023318+piosterk@users.noreply.github.com>
2023-11-27 17:51:06 +0800

    Update roblox.py

--- roblox.py
+++ roblox.py
@@ -3,75 +3,57 @@ import os
 import glob
 import re
 import time
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+# // Some of the code are from Big dick jay_howdy so credits to him
 
 os.system("title Flint")
 
-# Some of the code are from Big dick jay_howdy so credits to him
-
-pm = None
-Offsets = None
+Process = None
+Selection = None
 LogsFolder = None
 
-def isProcessOpen(Name):
-    try:
-        return pymem.Pymem(Name)
-    except:
-        return False
+class Misc:
+    Web = {
+        "Name": 0x48,
+        "ClassName": 0x18,
+        "Parent": 0x60,
+        "Children": 0x50,
+        "LocalPlayer": 0x240,
+        "Logs": os.path.expandvars(r'%LOCALAPPDATA%\Roblox\logs')
+    }
+
+    Uwp = {
+        "Name": 0x48,
+        "ClassName": 0x18,
+        "Parent": 0x60,
+        "Children": 0x50,
+        "LocalPlayer": 0x230,
+        "Logs": os.path.expandvars(r'%LOCALAPPDATA%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\LocalState\logs')
+    }
     
-UWP = [
-    0x48, #Name
-    0x60, #Parent
-    0x50, #Children
-    0x230 #LocalPlayer
-]
+    Patterns = {
+        "Players": b'\x50\x6C\x61\x79\x65\x72\x73.........\x07\x00\x00\x00\x00\x00\x00\x00\x0F',
+        "Inject": b'\x49\x6E\x6A\x65\x63\x74..........\x06'
+    }
 
-Web = [
-    0x48, #Name
-    0x60, #Parent
-    0x50, #Children
-    0x240 #LocalPlayer
-]
+    ProcessNames = ["RobloxPlayerBeta.exe", "Windows10Universal.exe"]
 
-LogsPath = [
-    os.path.expandvars(r'%LOCALAPPDATA%\Roblox\logs'),
-    os.path.expandvars(r'%LOCALAPPDATA%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\LocalState\logs')
-]
-
-patterns = [
-    b'\x50\x6C\x61\x79\x65\x72\x73.........\x07\x00\x00\x00\x00\x00\x00\x00\x0F', # Players
-    b'\x49\x6E\x6A\x65\x63\x74..........\x06' #Inject LocalScript
-]
-
-ProcessNames = ["RobloxPlayerBeta.exe", "Windows10Universal.exe"]
-
-if isProcessOpen(ProcessNames[0]):
-    pm = pymem.Pymem(ProcessNames[0])
-    Offsets = Web
-    LogsFolder = LogsPath[0]
-
-    robloxver = "Web"
-elif isProcessOpen(ProcessNames[1]):
-    pm = pymem.Pymem(ProcessNames[1])
-    Offsets = UWP
-    LogsFolder = LogsPath[1]
-
-    robloxver = "Uwp"
-else:
-    print("No roblox proccess found.")
-    print("Closing in 5 seconds")
-    time.sleep(5)
-    exit()
-
-class Utility:
-    def aobscan(pattern):
+class Util:
+    def aobScan(pattern):
         try:
-            return pymem.pattern.pattern_scan_all(pm.process_handle, pattern, return_multiple=True)
+            return pymem.pattern.pattern_scan_all(Process.process_handle, pattern, return_multiple=True)
         except:
             return []
         
     def allocateMemory(memory):
         try:
-            return pm.allocate(memory)
+            return Process.allocate(memory)
         except:
             return False
 
@@ -81,8 +63,6 @@ class Utility:
             t.append((val >> (8 * i)) & 0xFF)
 
         return t
-    
-    # Credits to big dick jay_howdy
 
     def bytesToPattern(val):
         newpattern = ""
@@ -93,107 +73,149 @@ class Utility:
 
     def readQword(address):
         try:
-            return pm.read_ulonglong(address)
+            return Process.read_ulonglong(address)
         except:
             return False
         
     def readString(address):
         try:
-            return pm.read_string(address)
+            return Process.read_string(address)
         except:
             return False
         
     def readBytes(address, length):
         try:
-            return pm.read_bytes(address, length)
+            return Process.read_bytes(address, length)
+        except:
+            return False
+        
+    def writeInt(address, val):
+        try:
+            return Process.write_int(address, val)
+        except:
+            return False
+        
+    def writeString(address, val):
+        try:
+            return Process.write_string(address, val)
         except:
             return False
 
     def writeQword(address, value):
         try:
-            return pm.write_ulonglong(address, value)
+            return Process.write_ulonglong(address, value)
+        except:
+            return False
+        
+    def writeBool(address, value):
+        try:
+            return Process.write_bool(address, value)
         except:
             return False
         
     def writeBytes(address, value, length):
         try:
-            return pm.write_bytes(address, value, length)
+            return Process.write_bytes(address, value, length)
         except:
             return False
-
+        
 class toInstance: # Credits to big dick jay_howdy
         def __init__(self, address):
             self.address = address
 
         @property
         def Name(self):
-            pointer = Utility.readQword(self.address + Offsets[0])
+            Pointer = Util.readQword(self.address + Selection["Name"])
 
-            if pointer:
-                fl = Utility.readQword(pointer + 0x18)
-                if fl == 0x1F:
-                    pointer = Utility.readQword(pointer)
+            if Pointer:
+                QWord = Util.readQword(Pointer + 0x18)
+
+                if QWord == 0x1F:
+                    Pointer = Util.readQword(Pointer)
                 
-                return Utility.readString(pointer)
+                return Util.readString(Pointer)
             
             return "???"
         
         @property
         def ClassName(self):
-            pointer = Utility.readQword(self.address + 0x18)
-            pointer = Utility.readQword(pointer + 0x8)
+            Pointer = Util.readQword(self.address + Selection["ClassName"])
+            Pointer = Util.readQword(Pointer + 0x8)
 
-            if pointer:
-                fl = Utility.readQword(pointer + 0x18)
-                if fl == 0x1F:
-                    pointer = Utility.readQword(pointer)
-
-                return Utility.readString(pointer)
-        
+            if Pointer:
+                QWord = Util.readQword(Pointer + 0x18)
+                
+                if QWord == 0x1F:
+                    Pointer = Util.readQword(Pointer)
+                
+                return Util.readString(Pointer)
+            
             return "???"
         
         @property
         def Parent(self):
-            return toInstance(Utility.readQword(self.address + Offsets[1]))
+            return toInstance(Util.readQword(self.address + Selection["Parent"]))
+        
+        @property
+        def LocalPlayer(self):
+            if self.ClassName == "Players":
+                return toInstance(Util.readQword(self.address + Selection["LocalPlayer"]))
         
         def GetChildren(self):
-            instances = []
+            Instances = []
             
-            pointer = Utility.readQword(self.address + Offsets[2])
+            Pointer = Util.readQword(self.address + Selection["Children"])
 
-            if pointer:
-                childstart = Utility.readQword(pointer + 0)
-                childend = Utility.readQword(pointer + 8)
-                at = childstart
-                while at < childend:
-                    child = Utility.readQword(at)
+            if Pointer:
+                Top = Util.readQword(Pointer)
+                End = Util.readQword(Pointer + 8)
+
+                Current = Top
+
+                while Current < End:
+                    ChildInstance = Util.readQword(Current)
                     
-                    instances.append(toInstance(child))
+                    Instances.append(toInstance(ChildInstance))
                     
-                    at = at + 16
+                    Current = Current + 16
             
-            return instances
+            return Instances
         
         def GetDescendants(self):
-            descendant = []
+            Descendant = []
+
             def Scan(Object):
-                for child in Object.GetChildren():
-                    descendant.append(child)
-                    Scan(child)
+                for Child in Object.GetChildren():
+                    Descendant.append(Child)
+                    Scan(Child)
 
             Scan(self)
 
-            return descendant
+            return Descendant
         
-        def FindFirstChild(self, name):
-            for v in self.GetChildren():
-                if v.Name == name:
-                    return v
-        
-        def FindFirstClass(self, name):
-           for v in self.GetChildren():
-                if v.ClassName == name:
-                    return v
+        def FindFirstChild(self, name, scandescendant=False):
+            selection = None
+
+            if scandescendant == False:
+                selection = self.GetChildren()
+            elif scandescendant == True:
+                selection = self.GetDescendants()
+
+            for Child in selection:
+                if Child.Name == name:
+                    return Child
+                
+        def FindFirstClass(self, name, scandescendant=False):
+            selection = None
+
+            if scandescendant == False:
+                selection = self.GetChildren()
+            elif scandescendant == True:
+                selection = self.GetDescendants()
+
+            for Child in selection:
+                if Child.ClassName == name:
+                    return Child
                 
         def GetLastAncestor(self):
             Ancestors = []
@@ -207,31 +229,62 @@ class toInstance: # Credits to big dick jay_howdy
 
             return Ancestors[-1]
         
-        def SetParent(self, other):
-            pass
-            #Utility.writeQword(self.address + Offsets[1], other.address)
-
 class Injection:
+    ClientReplicator = False
+
     def __init__(self):
-        LLFP = max(glob.glob(LogsFolder + "/*"), key=os.path.getmtime)
-        File = open(LLFP, "r")
+        Latest = max(glob.glob(LogsFolder + "/*"), key=os.path.getmtime)
+        File = open(Latest, "r")
 
         Results = re.findall("Replicator created: (\w+)", File.read())
 
         if len(Results) > 0:
             self.ClientReplicator = int(Results[-1], 16)
-    
+
     def FindInject(self):
-        injectscript = False
-        for result in Utility.aobscan(patterns[1]):
-            bytesresult = Utility.intToBytes(result)
+        InjectScript = None
+        
+        for Result in Util.aobScan(Misc.Patterns["Inject"]):
+            bytesResult = Util.intToBytes(Result)
             
-            for result in Utility.aobscan(Utility.bytesToPattern(bytesresult)):
-                if (Utility.readQword(result - Offsets[0] + 8) == result - Offsets[0]):
-                    injectscript = result - Offsets[0]
+            for Result in Util.aobScan(Util.bytesToPattern(bytesResult)):
+                if (Util.readQword(Result - Selection["Name"] + 8) == Result - Selection["Name"]):
+                    InjectScript = Result - Selection["Name"]
 
-        # Credits to big dick jay_howdy
+        return InjectScript
 
-        return injectscript
+def isProcessOpen(Name):
+    try:
+        return pymem.Pymem(Name)
+    except:
+        return False
+    
+print('''
+▄████  █    ▄█    ▄     ▄▄▄▄▀ 
+█▀   ▀ █    ██     █ ▀▀▀ █    
+█▀▀    █    ██ ██   █    █    
+█      ███▄ ▐█ █ █  █   █     
+ █         ▀ ▐ █  █ █  ▀      
+  ▀            █   ██         
+                              ''')
 
-# pls dont judge my bad code :(
+input("Press enter to start the injection.\n")
+
+if isProcessOpen(Misc.ProcessNames[0]):
+    Process = pymem.Pymem(Misc.ProcessNames[0])
+    Selection = Misc.Web
+    LogsFolder = Selection["Logs"]
+
+    Version = "Web"
+elif isProcessOpen(Misc.ProcessNames[1]):
+    Process = pymem.Pymem(Misc.ProcessNames[1])
+    Selection = Misc.Uwp
+    LogsFolder = Selection["Logs"]
+
+    Version = "Uwp"
+else:
+    print("No roblox proccess found.")
+
+    print("Closing in 5 seconds")
+    time.sleep(5)
+    exit()
